62e747980ba93afd525ac6848f0665385960932a
tux <tux@localhost.localdomain>
2020-08-08 11:40:09 +0700

    update groovy script

--- Pipelinescripted.groovy
+++ Pipelinescripted.groovy
@@ -1,7 +1,7 @@
 node
 {
     //Properties configure
-    properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '3', numToKeepStr: '3')), [$class: 'GithubProjectProperty', displayName: '', projectUrlStr: 'https://github.com/doan-nguyenduc/cLinux/'], pipelineTriggers([githubPush()])])
+    properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '3', numToKeepStr: '3')), [$class: 'GithubProjectProperty', displayName: '', projectUrlStr: 'https://github.com/doan-nguyenduc/cLinux/'], pipelineTriggers([githubPush(), pollSCM('* * * * *')])])
 	sh 'pwd'
     sh 'printenv'
     git 'https://github.com/doan-nguyenduc/cLinux.git'
