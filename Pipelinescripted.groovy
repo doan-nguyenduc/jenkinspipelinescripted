e1f38d830d124048ecc737a869f5346bb3fe8215
tux <tux@localhost.localdomain>
2020-08-08 10:47:32 +0700

    clone git source

--- Pipelinescripted.groovy
+++ Pipelinescripted.groovy
@@ -4,4 +4,5 @@ node
     properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '3', numToKeepStr: '3'))])
     sh 'pwd'
     sh 'printenv'
+    git 'https://github.com/doan-nguyenduc/cLinux.git'
 }
