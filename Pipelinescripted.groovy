dadffd20a8b825e025fb7421d5086d69522c31a6
tux <tux@localhost.localdomain>
2020-08-08 09:52:19 +0700

    Updated ls

--- Pipelinescripted.groovy
+++ Pipelinescripted.groovy
@@ -4,5 +4,7 @@ node
     properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '3', numToKeepStr: '3'))])
     sh 'pwd'
     sh 'printenv'
+    sh 'ls'
+    sh 'pwd'
     
 }
