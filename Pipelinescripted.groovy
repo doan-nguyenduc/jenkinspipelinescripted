832ba99feac562eeb2a9360a2962ab426c7ca0b2
tux <tux@localhost.localdomain>
2020-08-08 09:11:35 +0700

    Initial pipelinescripted

---
+++ Pipelinescripted.groovy
@@ -0,0 +1,8 @@
+node
+{
+    //Properties configure
+    properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '3', numToKeepStr: '3'))])
+    sh 'pwd'
+    sh 'printenv'
+    
+}
